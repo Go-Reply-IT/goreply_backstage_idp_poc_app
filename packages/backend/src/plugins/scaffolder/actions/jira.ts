92ed07fdb9546d9cec66115e1c2fb27a86f1edf4
Salvatore Fasano <fasanosalvatore@hotmail.it>
2023-11-16 21:01:15 +0100

    feat: add new parameters to Jira action

--- packages/backend/src/plugins/scaffolder/actions/jira.ts
+++ packages/backend/src/plugins/scaffolder/actions/jira.ts
@@ -9,41 +9,61 @@ export const createNewJiraTicketAction = () => {
     id: 'goreply:ticket:create',
     schema: {
       input: z.object({
-        title: z.string().describe('Title of the ticket'),
+        key: z.string().describe('ID of an already created ticket').optional(),
+        title: z.string().describe('Title of the ticket').optional(),
         description: z
           .string()
-          .describe('Description of the ticket'),
+          .describe('Description of the ticket').optional(),
+        type: z.string().describe('Type of the ticket').optional(),
+        status: z.string().describe('Status of the ticket').optional(),
       }),
       output: z.object({
         jiraIssueUrl: z.string().describe('Jira URL of the ticket'),
+        key: z.string().describe('ID of the ticket'),
       }),
 
     },
 
     async handler(ctx) {
       if (!jiraInstance) {
-        ctx.logger.log("error", "Jira non è stato confgiurato correttamente")
+        ctx.logger.log("error", "Jira has not been configured correctly.")
         return
       }
 
-      ctx.logger.log("info", "Creo un nuovo Ticket Jira...")
+      ctx.input.type = ctx.input.type == undefined ? "10563" : ctx.input.type
 
-      const issue = await jiraInstance.addNewIssue({
-        fields: {
-          project: {
-            key: "IDP"
-          },
-          summary: ctx.input.title,
-          description: ctx.input.description,
-          issuetype: {
-            name: "Bug"
+      ctx.logger.log("info", "I am declaring the task in Jira.")
+
+      let issue
+
+      if(!ctx.input.key) {
+        issue = await jiraInstance.addNewIssue({
+          fields: {
+            project: {
+              key: "IDP"
+            },
+            summary: ctx.input.title,
+            description: ctx.input.description,
+            issuetype: {
+              id: ctx.input.type
+            }
           }
+        })
+        ctx.output('jiraIssueUrl', `https://goreply.atlassian.net/browse/${issue.key}`)
+        ctx.output('key', issue.key)
+      } else {
+        if(!ctx.input.status) {
+          ctx.logger.log("error", "Unable to move the ticket, desired status not specified.")
+          return
         }
-      })
 
-      ctx.output('jiraIssueUrl', `https://goreply.atlassian.net/browse/${issue.key}`)
+        issue = await jiraInstance.transitionIssue(ctx.input.key, {
+          transition: {
+            id: ctx.input.status
+          }
+        })
 
-      ctx.logger.log("info", "Ticket creato con successo")
-    },
+      }
+    }
   });
 };
